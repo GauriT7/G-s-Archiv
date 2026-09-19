8645bfcd6f4daa0022ea98c998fbfe77748f25b5
GauriT7 <83664108+GauriT7@users.noreply.github.com>
2026-09-19 21:21:58 +0530

    Update script.js

--- seed01/concoction/script.js
+++ seed01/concoction/script.js
@@ -1,1 +1,20 @@
+const threadNodes = document.querySelectorAll(".thread-node");
 
+function revealThread() {
+
+    threadNodes.forEach((node, index) => {
+
+        const position = node.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (position < windowHeight * 0.75) {
+            node.classList.add("visible");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealThread);
+
+revealThread();
